ee7b043866538f15a4497aeb3bbfaa1d97e8dbe8
Bairon Núñez <lensko_2002@hotmail.com>
2025-11-18 16:05:52 -0300

    cambios en modulo

--- aeropuerto/src/app/app-routing.module.ts
+++ aeropuerto/src/app/app-routing.module.ts
@@ -1,29 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-// Componentes existentes
-import { LoginComponent } from './auth/login/login.component';
-import { CartComponent } from './cliente/cart/cart.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-
-// Guards
-import { AuthGuard } from './core/auth.guard';
-import { RoleGuard } from './core/role.guard';
-
-const routes: Routes = [
-
-  // Página inicial -> login
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-
-  { path: 'login', component: LoginComponent },
-  { path: 'cart', component: CartComponent, canActivate: [AuthGuard] },
-
-  // solo admin
-  { path: 'admin', component: DashboardComponent, canActivate: [RoleGuard] },
-
-  // cualquier ruta desconocida -> login
-  { path: '**', redirectTo: '/login' }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
