d7b02775f2aae1a13d5da0c3f0781a1a1128fd12
Kendyzinho <rafakendy2003@gmail.com>
2025-11-17 23:40:44 -0300

    Ventana del carrito
    Una ventana de compras de vuelos.

--- aeropuerto/src/app/app.module.ts
+++ aeropuerto/src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; // para el two-way binding [(ngModel)]
+import { FormsModule } from '@angular/forms'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 // IMPORTS DE COMPONENTES
-import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
-
 import { LoginComponent } from './auth/login/login.component';
 import { CartComponent } from './cliente/cart/cart.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
@@ -17,11 +15,8 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     FooterComponent,
     HeaderComponent,
-
-    // Pages
     LoginComponent,
     CartComponent,
     DashboardComponent
