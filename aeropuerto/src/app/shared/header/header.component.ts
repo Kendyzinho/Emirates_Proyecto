ef5e658c29f7a4ce64acc6a0128e25fabcff4997
Kendyzinho <rafakendy2003@gmail.com>
2025-11-17 23:06:22 -0300

    Revert "version estable"
    This reverts commit 1271331794c2fde8452fa54c052a63f793b88ad5.

--- aeropuerto/src/app/shared/header/header.component.ts
+++ aeropuerto/src/app/shared/header/header.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
+  imports: [],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
