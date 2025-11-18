7ad256c9ee3e0572f2c391f6fc6dc5640ffdce09
Bairon Núñez <lensko_2002@hotmail.com>
2025-11-17 21:53:37 -0300

    cambios en header

--- aeropuerto/src/app/shared/header/header.component.ts
+++ aeropuerto/src/app/shared/header/header.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
 
